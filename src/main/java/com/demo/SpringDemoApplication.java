316fc2ea38cf6242824bb0098043bff5564b0aae
hushtar <HUXTAR.EZ@gmail.com>
2024-08-12 21:00:41 +0200

    Initial commit

---
+++ src/main/java/com/demo/SpringDemoApplication.java
@@ -0,0 +1,50 @@
+package com.demo;
+
+import com.demo.bean.Car;
+import com.demo.config.ProjectConfig;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootApplication
+public class SpringDemoApplication {
+
+    public static void main(String[] args) {
+        //SpringApplication.run(SpringDemoApplication.class, args);
+        var bn1= new AnnotationConfigApplicationContext(ProjectConfig.class);
+        var machine=bn1.getBean( "Acar",Car.class);
+        System.out.println(machine.getName());
+
+        var bn2=new AnnotationConfigApplicationContext(ProjectConfig.class);
+        var strings=bn2.getBean("hello", String.class);
+        System.out.println(strings);
+
+        var bn3=new AnnotationConfigApplicationContext(ProjectConfig.class);
+        var num=bn3.getBean("number", Integer.class);
+        System.out.println(num);
+
+        var bn4=new AnnotationConfigApplicationContext(ProjectConfig.class);
+        //var machine2=bn4.getBean(Car.class);
+        /* -> error
+         because in that main class have diff method,
+         we should specify which method in that class we're going to use
+         or getting a default bean which include @Primary annotation*/
+        var machine2=bn4.getBean("Bcar",Car.class);
+        System.out.println(machine2.getName());
+
+        var bn5=new AnnotationConfigApplicationContext(ProjectConfig.class);
+        //var machine3=bn5.getBean( Car.class);
+        /* -> error
+         because in that main class have diff method,
+         we should specify which method in that class we're going to use
+         or getting a default bean which include @Primary annotation*/
+        var machine3=bn5.getBean( Car.class); //default bean , no name
+        System.out.println(machine3.getName());
+
+
+        var bn6=new AnnotationConfigApplicationContext(ProjectConfig.class);
+        var machine4=bn6.getBean( Car.class);
+        System.out.println(machine4.getName());
+    }
+
+}
---
+++ src/main/java/com/demo/config/ProjectConfig.java
@@ -0,0 +1,53 @@
+package com.demo.config;
+
+import com.demo.bean.Car;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration  //Scan all the content of this class
+/*
+Spring Configuration annotation indicates that the class has @Bean definition
+methods. So Spring container can process the class and generate Spring Beans
+to be used in the application.
+*/
+public class ProjectConfig {
+    @Bean(name="Acar")
+    Car car (){
+        var car = new Car();
+        car.setName("Audi");
+        return car;
+    }
+
+    @Bean(value="Bcar")
+    Car car2 (){
+        var car = new Car();
+        car.setName("Benz");
+        return car;
+    }
+
+    @Primary
+    //The Primary bean is the one which Spring will choose if it has multiple options and didn't specify a name.
+    @Bean
+    Car car3 (){
+        var car = new Car();
+        car.setName("Bmw");
+        return car;
+    }
+
+    @Bean
+    String hello() {
+        return "Hello World";
+    }
+
+    @Bean
+    Integer number() {
+        return 17;
+    }
+
+    /*------------------------------------------------------------------------------------------------*/
+
+
+
+
+}
